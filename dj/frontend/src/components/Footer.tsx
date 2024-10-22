b38542dde476da79fc3df95ab6096489fc954fb5
caner <canercetin853@gmail.com>
2024-10-22 17:38:17 +0300

    very, very small change in footer.

--- dj/frontend/src/components/Footer.tsx
+++ dj/frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@
 export const Footer = () => {
   return (<footer className="mt-auto py-4 text-center text-sm">
     <p>
-      Due to copyright issues, there is only one artist and album uploaded, so this site is nothing but a cool demo.
+      Due to copyright issues, there is only few selected albums uploaded.
     </p>
   </footer>)
 }
