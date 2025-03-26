d3785d1f44fd203868dbcbbec09e1fb634de7226
caner <canercetin853@gmail.com>
2025-03-26 03:52:45 +0300

    static: fuck bot protection, cloudflare has my back (hopefully)

--- deployments/static/setup.sh
+++ deployments/static/setup.sh
@@ -33,12 +33,6 @@ add_user_if_not_exists static_cansu_dev_user static_cansu_dev_group /var/www/ser
 chown -R static_cansu_dev_user:static_cansu_dev_group /var/www/servers/cansu.dev/static/
 chmod -R 755 /var/www/servers/cansu.dev/static/
 
-mkdir -p /etc/nginx/bots.d
-
-wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/blockbots.conf -O /etc/nginx/bots.d/blockbots.conf 
-wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/ddos.conf -O /etc/nginx/bots.d/ddos.conf
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/conf.d/globalblacklist.conf" -O "/etc/nginx/conf.d/globalblacklist.conf"
-
 chown -R static_cansu_dev_user:static_cansu_dev_group /var/log/nginx
 chmod -R 755 /var/log/nginx
 
