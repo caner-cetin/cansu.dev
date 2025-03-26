d366031e433e5d140656f48a8717467f1c980321
caner <canercetin853@gmail.com>
2025-03-26 03:47:49 +0300

    static: hmmm...

--- deployments/static/setup.sh
+++ deployments/static/setup.sh
@@ -14,13 +14,8 @@ chmod -R 755 /var/www/servers/cansu.dev/static/
 
 mkdir -p /etc/nginx/bots.d
 
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/blockbots.conf" -O "/etc/nginx/bots.d/blockbots.conf"
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/ddos.conf" -O "/etc/nginx/bots.d/ddos.conf"
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/blacklist-user-agents.conf" -O "/etc/nginx/bots.d/blacklist-user-agents.conf"
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/custom-bad-referrers.conf" -O "/etc/nginx/bots.d/custom-bad-referrers.conf"
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/blacklist-ips.conf" -O "/etc/nginx/bots.d/blacklist-ips.conf"
-wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/bad-referrer-words.conf" -O "/etc/nginx/bots.d/bad-referrer-words.conf"
-
+wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/blockbots.conf -O /etc/nginx/bots.d/blockbots.conf 
+wget https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/bots.d/ddos.conf -O /etc/nginx/bots.d/ddos.conf
 wget "https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/conf.d/globalblacklist.conf" -O "/etc/nginx/conf.d/globalblacklist.conf"
 
 chown -R static_cansu_dev_user:static_cansu_dev_group /var/log/nginx
